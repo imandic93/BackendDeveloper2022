d755ef9b74b9523acc94f32d262b1af33275551d
Ivan Mandic <ivan.mandic@predavaci.algebra.hr>
2022-12-20 19:43:13 +0000

    Funkcije

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Uvod u PHP</title>
-</head>
-<body>
-<pre>
 <?php
 
-$names = ['Ivan', 'Tony', 'Josip', 'Kristijan', 'Igor'];
+function getNameAndSurnameAddedTogether($name, $surname)
+{
+    $nameAndSurname = $name . ' ' . $surname;
+    $nameAndSurname = mb_strtoupper($nameAndSurname);
 
-foreach($names as $key => $name){
-    echo "Ključ: $key, Ime: $name <br>" ;
-} 
-  
-?>
-</pre>
-</body>
-</html>
+    return $nameAndSurname;
+}
+
+$text = getNameAndSurnameAddedTogether('Ivan', 'Mandić');
+echo "$text\n";
---
+++ vjezbe/Funkcije/vjezba2.php
@@ -0,0 +1,10 @@
+<?php
+// Deklariranje funkcije
+function fullName($name, $surname){
+    $fullName = $name . ' ' . $surname;
+    return strtoupper($fullName);
+}
+// Dodjeljivanje vrijednosti iz funkcije varijabli
+$fullName = fullName('Ivan', 'Horvat');
+// Ispis vrijednosti varijable
+echo $fullName;
